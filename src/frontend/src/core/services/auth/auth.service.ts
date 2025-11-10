fa031fc329492ba0d80dc6425029dee0bb16ff82
Vitor Machado <vitor_machado02@outlook.com>
2025-11-10 17:41:17 -0300

    :hammer:

--- src/frontend/src/core/services/auth/auth.service.ts
+++ src/frontend/src/core/services/auth/auth.service.ts
@@ -9,8 +9,8 @@ import { User } from 'core/models/user.models';
 })
 export class AuthService {
   // Configuração
-  private readonly apiUrl = "http://localhost:8080/auth"; 
-  // private readonly apiUrl = 'http://44.220.148.12:8080/auth';
+  // private readonly apiUrl = "http://localhost:8080/auth"; 
+  private readonly apiUrl = 'http://44.220.148.12:8080/auth';
   private readonly TOKEN_KEY = 'auth_token';
   private readonly USER = 'user';
 
